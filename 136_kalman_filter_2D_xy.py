2de3532b69b789fbe456925a5b72ae4fc2f6d981
Tony Yang <tianyuyang658@yahoo.com>
2017-01-18 07:27:22 -0800

    Complete the 2D Kalman filter example

--- 136_kalman_filter_2D_xy.py
+++ 136_kalman_filter_2D_xy.py
@@ -121,31 +121,61 @@ class matrix:
 
 def filter(x, P):
     for n in range(len(measurements)):
-        # measurement update
-        y = matrix([[measurements[n]]]) - H * x
-        S = H * P * H.transpose() + R
-        K = P * H.transpose() * S.inverse()
-        x = x + K * y
-        P = (I - K * H) * P
-
         # prediction
         x = F * x + u
         P = F * P * F.transpose()
+
+        # measurement update
+        Z = matrix([measurements[n]])
+        y = Z.transpose() - (H * x)
+        S = H * P * H.transpose() + R
+        K = P * H.transpose() * S.inverse()
+        x = x + (K * y)
+        P = (I - (K * H)) * P
 
         print('x= ')
         x.show()
         print('P= ')
         P.show()
 
-measurements = [1, 2, 3]
+measurements = [[5., 10.], [6., 8.], [7., 6.], [8., 4.], [9., 2.], [10., 0.]]
+initial_xy = [4., 12.]
 
-x = matrix([[0.], [0.]]) # initial state (location and velocity)
-P = matrix([[1000., 0.], [0., 1000.]]) # initial uncertainty
-u = matrix([[0.], [0.]]) # external motion
-F = matrix([[1., 1.], [0, 1.]]) # next state function
-H = matrix([[1., 0.]]) # measurement function
-R = matrix([[1.]]) # measurement uncertainty
-I = matrix([[1., 0.], [0., 1.]]) # identity matrix
+dt = 0.1
 
+x = matrix([[initial_xy[0]], [initial_xy[1]], [0.], [0.]]) # initial state (location and velocity)
+u = matrix([[0.], [0.], [0.], [0.]]) # external motion
+P = matrix([[0., 0., 0., 0.],
+            [0., 0., 0., 0.],
+            [0., 0., 1000., 0.],
+            [0., 0., 0., 1000.]]) # initial uncertainty
+F = matrix([[1., 0., dt, 0.],
+            [0., 1., 0., dt],
+            [0., 0., 1., 0],
+            [0., 0., 0., 1.]]) # next state function
+H = matrix([[1., 0., 0., 0.],
+            [0, 1., 0., 0.]]) # measurement function
+R = matrix([[0.1, 0.], [0., 1.0]]) # measurement uncertainty
+I = matrix([[1., 0., 0., 0.],
+            [0., 1., 0., 0.],
+            [0., 0., 1., 0.],
+            [0., 0., 0., 1.]]) # identity matrix
+
+
+filter(x, P)
+
+print("===========Measurement 2 ================")
+measurements = [[1., 4.], [6., 0.], [11., -4.], [16., -8.]]
+initial_xy = [-4., 8.]
+
+dt = 0.1
+
+x = matrix([[initial_xy[0]], [initial_xy[1]], [0.], [0.]]) # initial state (location and velocity)
+u = matrix([[0.], [0.], [0.], [0.]]) # external motion
+P = matrix([[0., 0., 0., 0.], [0., 0., 0., 0.], [0., 0., 1000., 0.], [0., 0., 0., 1000.]]) # initial uncertainty
+F = matrix([[1., 0., dt, 0.], [0., 1., 0., dt], [0., 0., 1., 0], [0., 0., 0., 1.]]) # next state function
+H = matrix([[1., 0., 0., 0.], [0, 1., 0., 0.]]) # measurement function
+R = matrix([[0.1, 0.], [0., 1.0]]) # measurement uncertainty
+I = matrix([[1., 0., 0., 0.], [0., 1., 0., 0.], [0., 0., 1., 0.], [0., 0., 0., 1.]]) # identity matrix
 
 filter(x, P)
