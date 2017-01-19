b78d3ed9c5c00732d3998d99c5feb9765000e138
Tony Yang <tianyuyang658@yahoo.com>
2017-01-19 08:52:56 -0800

    Particle filter with resampling

---
+++ 159_particle_filter.py
@@ -0,0 +1,138 @@
+from math import *
+import random
+
+landmarks = [[20.0, 20.0],
+             [80.0, 80.0],
+             [20.0, 80.0],
+             [80.0, 20.0]]
+world_size = 100.0
+
+class robot:
+    def __init__(self):
+        self.x = random.random() * world_size
+        self.y = random.random() * world_size
+        self.orientation = random.random() * 2.0 * pi
+        self.forward_noise = 0.0
+        self.turn_noise = 0.0
+        self.sense_noise = 0.0
+
+    def set(self, new_x, new_y, new_orientation):
+        if new_x < 0 or new_x >= world_size:
+            raise ValueError('X coordinate out of bound')
+        if new_y < 0 or new_y >= world_size:
+            raise ValueError('Y coordinate out of bound')
+        if new_orientation < 0 or new_orientation >= 2 * pi:
+            raise ValueError('Orientation must be in [0..2pi]')
+        self.x = float(new_x)
+        self.y = float(new_y)
+        self.orientation = float(new_orientation)
+
+    def set_noise(self, new_forward_noise, new_turn_noise, new_sense_noise):
+        self.forward_noise = float(new_forward_noise)
+        self.turn_noise = float(new_turn_noise)
+        self.sense_noise = float(new_sense_noise)
+
+    def sense(self):
+        Z = []
+        for i in range(len(landmarks)):
+            dist = sqrt((self.x - landmarks[i][0]) ** 2 + (self.y - landmarks[i][1]) ** 2)
+            dist += random.gauss(0.0, self.sense_noise)
+            Z.append(dist)
+        return Z
+
+    def move(self, turn, forward):
+        if forward < 0:
+            raise ValueError('Robot cannot move backwards')
+
+        orientation = self.orientation + float(turn) + random.gauss(0.0, self.turn_noise)
+        orientation %= 2 * pi
+
+        dist = float(forward) + random.gauss(0.0, self.forward_noise)
+        x = self.x + (cos(orientation) * dist)
+        y = self.y + (sin(orientation) * dist)
+        x %= world_size
+        y %= world_size
+
+        res = robot()
+        res.set(x, y, orientation)
+        res.set_noise(self.forward_noise, self.turn_noise, self.sense_noise)
+        return res
+
+    def Gaussian(self, mu, sigma, x):
+        return exp(- ((mu - x) ** 2) / (sigma ** 2) / 2.0) / sqrt(2.0 * pi * (sigma ** 2))
+
+    def measurement_prob(self, measurement):
+        prob = 1.0
+        for i in range(len(landmarks)):
+            dist = sqrt((self.x - landmarks[i][0]) ** 2 + (self.y - landmarks[i][1]) ** 2)
+            prob *= self.Gaussian(dist, self.sense_noise, measurement[i])
+        return prob
+
+    def __repr__(self):
+        return '[x={} y={} orientation={}]\n'.format(str(self.x), str(self.y), str(self.orientation))
+
+myrobot = robot()
+myrobot = myrobot.move(0.1, 5.0)
+Z = myrobot.sense()
+print(Z)
+print(myrobot)
+
+N = 1000
+T = 10
+
+p = []
+for i in range(N):
+    r = robot()
+    r.set_noise(0.05, 0.05, 5.0)
+    p.append(r)
+
+for t in range(T):
+    p2 = []
+    for i in range(N):
+        p2.append(p[i].move(0.1, 5.0))
+    p = p2
+
+    w = []
+    for i in range(N):
+        w.append(p[i].measurement_prob(Z))
+
+    print(len(p))
+
+    p3 = []
+    # Inefficient implementation
+    # normalized_aggregated_weight = []
+    # total_weight = sum(w)
+    # normalized_aggregated_weight.append(w[0] / total_weight)
+    # for i in range(N-1):
+    #     weight = normalized_aggregated_weight[i] + w[i + 1] / total_weight
+    #     normalized_aggregated_weight.append(weight)
+    #
+    # for i in range(N):
+    #     choice = random.random()
+    #     particle_index = N
+    #     for i in range(N):
+    #         if choice < normalized_aggregated_weight[i]:
+    #             particle_index = i - 1
+    #             break
+    #
+    #     if particle_index < 0:
+    #         particle_index = 0
+    #
+    #     p3.append(p[particle_index])
+
+
+    ## More efficient way
+    p3 = []
+    index = random.randint(0, N-1)
+    beta = 0.0
+    weight_max = max(w)
+    for i in range(N):
+        beta += random.uniform(0, 2 * weight_max)
+        while w[index] < beta:
+            beta -= w[index]
+            index = (index + 1) % N
+
+        p3.append(p[index])
+    p = p3
+
+print(p)
