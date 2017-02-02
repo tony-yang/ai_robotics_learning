cc52e0eabf5c22847e8ee98cedda3a926dbe963d
Tony Yang <tianyuyang658@yahoo.com>
2017-02-02 08:34:46 -0800

    My implementation for the value program

---
+++ 231_dynamic_programming_value_program.py
@@ -0,0 +1,60 @@
+from collections import deque
+
+grid = [[0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0],
+        [0, 0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 1, 0]]
+
+init = [0, 0]
+
+goal = [len(grid) - 1, len(grid[0]) - 1]
+
+delta = [[-1, 0], # up
+         [0, -1], # left
+         [1, 0], # down
+         [0, 1]] # right
+
+delta_name = ['^', '<', 'v', '>']
+cost_step = 1
+goal_state_cost = 0
+
+value = [[99 for item in row] for row in grid]
+grid_position_i = goal[0]
+grid_position_j = goal[1]
+value[grid_position_i][grid_position_j] = goal_state_cost
+new_open_list = deque()
+new_open_list.append([grid_position_i, grid_position_j])
+visited_position = {}
+
+def valid_position(i, j, visited_position, new_open_list):
+    if (i >= 0 and i < len(grid) and j >= 0 and j < len(grid[0]) and
+        grid[i][j] != 1 and
+        '{},{}'.format(i, j) not in visited_position and
+        [i, j] not in new_open_list):
+        return True
+    return False
+
+def expand_children(state, current_state_cost, visited_position, new_open_list):
+    for i in range(len(delta)):
+        new_position_i = state[0] + delta[i][0]
+        new_position_j = state[1] + delta[i][1]
+        if valid_position(new_position_i, new_position_j, visited_position, new_open_list):
+            value[new_position_i][new_position_j] = current_state_cost + cost_step
+            new_open_list.append([new_position_i, new_position_j])
+
+while new_open_list:
+    explore_state = new_open_list.popleft()
+#    print('----- explore state')
+#    print(explore_state)
+    state_cost = value[explore_state[0]][explore_state[1]]
+    expand_children(explore_state, state_cost, visited_position, new_open_list)
+    visited_position['{},{}'.format(explore_state[0], explore_state[1])] = 1
+#    print('open_list')
+#    print('    ', new_open_list)
+#    print('visited position')
+#    print('    ', visited_position)
+
+for i in range(len(value)):
+    print(value[i])
